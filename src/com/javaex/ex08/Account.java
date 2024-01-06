d95c98d1b3d11f524f6846afc555685cb907173f
poula9744 <poula9744@naver.com>
2024-01-06 20:21:19 +0900

    1st

--- src/com/javaex/ex08/Account.java
+++ src/com/javaex/ex08/Account.java
@@ -4,9 +4,41 @@ public class Account {
 
     private String accountNo;
     private int balance;
-    
+	
     //생성자 작성
-    
-    //필요한 메소드 작성
+    public Account(String accountNo) {
+		this.accountNo = accountNo;
+	}
 
+	
+    //필요한 메소드 작성
+    //메소드 - g/s
+    public String getAccountNo() {
+		return accountNo;
+	}
+
+	public void setAccountNo(String accountNo) {
+		this.accountNo = accountNo;
+	}
+
+	public int getBalance() {
+		return balance;
+	}
+
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+
+	//메소드 - 일반
+	public void deposit(int balance) {
+		this.balance += balance;
+	}
+
+	public void withdraw(int balance) {
+		this.balance -= balance;
+	}
+	public void showBalance() {
+		System.out.println(balance);
+	}
+	
 }
