54c20aeddf7f0efe800c866defb52a3fa4eeb930
poula9744 <poula9744@naver.com>
2024-01-07 19:25:29 +0900

    1st

--- src/com/javaex/ex09/StringUtil.java
+++ src/com/javaex/ex09/StringUtil.java
@@ -2,10 +2,15 @@ package com.javaex.ex09;
 
 public class StringUtil {
     
-    public static String concatString(){
+    public static String concatString(String[] strArray){
        
+    	String sum = "";
         //메소드 내용작성
-        
+        for(int i = 0; i<strArray.length; i++) {
+        	
+        	sum += strArray[i];
+        }
+        return sum;
     }
 
 }
